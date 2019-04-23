94ce6e82d21b8681c6a2d1bb4d90b7ea368eb59d
LeChenHz <405780108@qq.com>
2019-04-23 18:04:46 +0800

    first commit

---
+++ Assets/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject MC;
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        LoadDLL mc = MC.GetComponent<LoadDLL>();
+        //if(1)
+        mc.threshold *= 1.01f;
+        mc.Reflash();
+        //if (1)
+           // mc.Half();
+
+    }
+}
---
+++ Assets/LoadDLL.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class LoadDLL : MonoBehaviour
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct CPPMesh
+    {
+        public uint VerticesCount;
+        public IntPtr Vertices;
+        public IntPtr Normals;
+    };
+
+    public float threshold = 20000.0f;
+    public string FileName;
+
+    [DllImport("MarchingCubeCPP")]
+    private static extern void InitMarchingCube(IntPtr inData, ushort x, ushort y, ushort z);
+
+    [DllImport("MarchingCubeCPP")]
+    private static extern void MarchingCubeIsosurface(ref CPPMesh mesh, float target_value);
+
+    private volatile bool isInit = false;
+    private volatile bool isWorking = false;
+
+
+    public void Double()
+    {
+        threshold *= 2.0f;
+    }
+
+    public void Half()
+    {
+        threshold *= 0.5f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset asset = Resources.Load<TextAsset>(FileName);
+        if(asset == null)
+        {
+            Debug.LogError("File Do not Exists");
+            return;
+        }
+        var allBytes = asset.bytes;
+        var data = Marshal.UnsafeAddrOfPinnedArrayElement(allBytes, 0);
+        InitMarchingCube(data,200,160,160);
+        isInit = true;
+        OnValidate();
+    }
+
+    public void Reflash() => OnValidate();
+
+    void OnValidate()
+    {
+        if (isWorking || !isInit)
+        {
+            return;
+        }
+        float oldT = threshold;
+        isWorking = true;
+        CPPMesh mesh = new CPPMesh();
+        MarchingCubeIsosurface(ref mesh, oldT);
+
+        int len = (int)(mesh.VerticesCount * 3);
+        float[] vertices = new float[len];
+        float[] normals = new float[len];
+        Marshal.Copy(mesh.Vertices, vertices, 0, len);
+        Marshal.Copy(mesh.Normals, normals, 0, len);
+
+        Mesh unityMesh = new Mesh();
+        Vector3[] meshV = new Vector3[mesh.VerticesCount];
+        Vector3[] meshN = new Vector3[mesh.VerticesCount];
+        int[] meshT = new int[mesh.VerticesCount];
+        for (int i = 0; i < mesh.VerticesCount; i++) meshT[i] = i;
+        for (int i = 0; i < mesh.VerticesCount; i++)
+        {
+            meshV[i] = new Vector3(vertices[3 * i + 0], vertices[3 * i + 1], vertices[3 * i + 2]);
+            meshN[i] = new Vector3(normals[3 * i + 0], normals[3 * i + 1], normals[3 * i + 2]);
+        }
+        unityMesh.vertices = meshV;
+        unityMesh.normals = meshN;
+        unityMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        unityMesh.triangles = meshT;
+        gameObject.GetComponent<MeshFilter>().mesh = unityMesh;
+        isWorking = false;
+        if(oldT != threshold)
+        {
+            OnValidate();
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        gameObject.GetComponent<MeshFilter>().mesh = null;
+        //Destroy(gameObject.GetComponent<MeshFilter>());
+    }
+}
